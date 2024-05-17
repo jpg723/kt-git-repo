8f789b4c2c739bf41c62cc77734a248f36584de7
MiJeeLee <dlalwl723@naver.com>
2024-05-17 12:11:12 +0900

    [kt-aivle] 1차 미니프로젝트

--- Django/mysite/blog/views.py
+++ Django/mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # ~/blog/test1/ 요청이 들어왔을 때 실행할 View 객체
 
-def test1():
-    pass
+def test1(request):
+    # 서비스 구현
+    return HttpResponse('test1!')
